569daff013c7f61c8dc0ca18775686b892fbb9ea
Maritza A. Loaiza Urrutia <32825151+Maritza7395@users.noreply.github.com>
2020-01-03 10:55:39 -0300

    Update cuit-cuil-argentina.js
    agregando comentarios

--- cuit-cuil-argentina.js
+++ cuit-cuil-argentina.js
@@ -1,4 +1,6 @@
+//Recibiendo el parámetro "cuit" desde un input
 validation_Cuil_Cuit(cuit) {
+    //limpiamos el input en caso de que el usuario ingrese "-"
     cuit = cuit.replace(/[^\w]/g, "");
     if(cuit.length != 11){
       return false
@@ -13,6 +15,20 @@ validation_Cuil_Cuit(cuit) {
     if(verif == 11){
       verif = 0
     }
-    this.destino.rutUser = cuit.replace(/(\d{2})(\d{8})(\d{1})/g, "\$1-\$2-\$3")
+    //Dando formato en pantalla
+    this.variable = cuit.replace(/(\d{2})(\d{8})(\d{1})/g, "\$1-\$2-\$3")
+    
     return digito == verif;
   }
+
+//CUIT y CUIL de pruebas
+//   33-37145386-0
+//   33371453860
+//   20172543597
+//   20131048131
+//   20-48028763-1
+//   33-25497340-3 
+//   33-35036407-8
+//   20-12145175-2
+//   33-37145386-0
+//   20-13104813-3
