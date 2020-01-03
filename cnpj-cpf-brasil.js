a0309bc47139218dc50a2d131caac18b669ba34d
Maritza A. Loaiza Urrutia <32825151+Maritza7395@users.noreply.github.com>
2020-01-03 11:31:09 -0300

    comentarios

--- cnpj-cpf-brasil.js
+++ cnpj-cpf-brasil.js
@@ -96,11 +96,12 @@ validation_CNPJ_CPF(cnpj) {
       return false;
     }
   } 
+
+
     //CNPJs y CPFs de pruebas
     //******CNPJs Validos********
     //    29.083.780/4721-70
     //    16823872289165
-
     //******CPFs Validos*********
     //    85535084557
     //    784.047.332-60
