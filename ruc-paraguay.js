7b7757672a0f0931a598766f35e1e9d47f2c6ba6
Maritza A. Loaiza Urrutia <32825151+Maritza7395@users.noreply.github.com>
2020-01-03 11:43:27 -0300

    comentarios

--- ruc-paraguay.js
+++ ruc-paraguay.js
@@ -1,3 +1,6 @@
+public validRut: boolean;
+this.validRut = true;
+
 //this.variable es la variable que hace referencia al input
 let cuerpo = this.variable.replace(/[^\w]/g, "").slice(0, -1);
 
@@ -5,11 +8,19 @@ let cuerpo = this.variable.replace(/[^\w]/g, "").slice(0, -1);
 let dv = this.validation_ruc_paraguay(cuerpo)
 
 //se valida si el digito verificador es igual al ingresado en el input por el usuario
-this.validRut = (dv ==  this.destino.rutUser.slice(-1))? false: true;
+this.validRut = (dv ==  this.variable.slice(-1))? true: false;
+
+//se le da formato XX.XXX.XXX-Y al input si el largo es 8 
+if(cuerpo.length == 8){
+    this.variable = this.destino.rutUser.replace(/(\d{8})(\d{1})/g, "\$1-\$2
+} else{
+    //se le da formato X.XXX.XXX-Y al input si el largo es 7                      
+    this.variable = this.destino.rutUser.replace(/(\d{7})(\d{1})/g, "\$1-\$2");
+}
 
 //************************************************************************************************
 
-//Recibiendo el parámetro desde el input
+
 validation_ruc_paraguay(p_numero, p_basemax = 11){
     //Eliminando cualquier caracter ingresado por el usuario
     p_numero = p_numero.replace(/[^\w]/g, '');
@@ -38,3 +49,15 @@ validation_ruc_paraguay(p_numero, p_basemax = 11){
     };
     return v_digit;
   }
+
+//RUC de pruebas
+//******Validos********
+//    80009837-4
+//    80044453-1
+//    80004761-3
+//    80002269-6
+//    1946520-3
+//****Invalidos********
+//    80009837-5
+//    80044453-6
+
