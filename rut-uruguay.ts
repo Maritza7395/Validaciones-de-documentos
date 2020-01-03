8fc5afb6a460e3ee7ee9d1d984ee899fe8af3cdc
Maritza A. Loaiza Urrutia <32825151+Maritza7395@users.noreply.github.com>
2020-01-03 11:49:24 -0300

    comentarios

--- rut-uruguay.ts
+++ rut-uruguay.ts
@@ -1,11 +1,14 @@
-  //Validando el RUT de Uruguay
+  //Recibiendo el parámetro "dato" desde le input
   validation_rut_uruguay(dato) {
+    //Eliminando caracteres especiales
     dato = dato.replace(/[^\w]/g, '');
     let cuerpo = dato.slice(0, -1);
+    //recibiendo el digito verificador desde la funcion
     let resultadoDv = this.validation_digit_uruguay(cuerpo);
     let dv = dato.slice(-1);
     cuerpo = cuerpo.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
     this.destino.rutUser = cuerpo + '-' + dv;
+    //verificando si el digito verificador es igual al ingresado
     if (resultadoDv != dv) {
       return false;
     }
@@ -31,3 +34,13 @@
       return 10 - a % 10;
     }
   }
+
+//RUT de pruebas
+//******Validos********
+//    9.999.999-9
+//    4.907.760-2
+//    1.234.567-2
+//*****Invalidos*******
+//    9.999.999-6
+//    4.907.760-5
+//    1.234.567-3
